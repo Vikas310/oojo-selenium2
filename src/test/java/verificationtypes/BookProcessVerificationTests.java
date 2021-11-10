6d573234039eacd537c4108e0ecc374d0432af8a
Andris Zolovkins <a.zolovkins@dynatech.lv>
2021-11-10 08:54:12 +0200

    Add after method

--- src/test/java/verificationtypes/BookProcessVerificationTests.java
+++ src/test/java/verificationtypes/BookProcessVerificationTests.java
@@ -5,6 +5,7 @@ import constants.Constants;
 import constants.FlightCodes;
 import constants.TestData;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -282,6 +283,11 @@ public class BookProcessVerificationTests extends BaseTest {
         headerPageObject.waitForVerificationSpinnerToStop();
         logWrite.info("Check if take photo screen is present");
         Assert.assertTrue(bookSuccessPageObject.getCntTakePhotoSuccessMessage().isDisplayed());
+    }
+
+    @AfterMethod
+    public void quitDriver() {
+        driver.quit();
     }
 
 }

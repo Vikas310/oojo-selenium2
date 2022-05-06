a98f861269e60b6c300932bd0eec5495ee09f066
Andris Zolovkins <a.zolovkins@dynatech.lv>
2022-05-06 13:15:43 +0300

    added 3 more flight search result check cache segments

--- src/test/java/frss/TakeSegmentCacheTests.java
+++ src/test/java/frss/TakeSegmentCacheTests.java
@@ -37,7 +37,7 @@ public class TakeSegmentCacheTests extends BaseTest {
     }
 
     @Test
-    public void searchFlightAndOpenSavedUrlFromNewYorkToLosAngelos() {
+    public void searchFlightAndOpenSavedUrlFromNewYorkToLosAngelosAndOpenCachedUrl() {
 
         int flight = 0;
 
@@ -87,7 +87,7 @@ public class TakeSegmentCacheTests extends BaseTest {
     }
 
     @Test
-    public void searchFlightAndOpenSavedUrlFromNewYorkToMiami() {
+    public void searchFlightAndOpenSavedUrlFromNewYorkToMiamiAndOpenCachedUrl() {
 
         int flight = 0;
         String customDate = Helper.getDateWithSpecificMonthsInFuture(Constants.FOUR_MONTHS,"yyyy-MM-dd");
@@ -132,6 +132,151 @@ public class TakeSegmentCacheTests extends BaseTest {
         logWrite.info("Check that price from the search result matches the price on the book screen " + totalPriceSecondTime);
         Assert.assertEquals(totalPriceSecondTime,pQFlightPrice, "Price from flight form search result does not match price from book screen");
 
+    }
+
+    @Test
+    public void searchDallasToLahoreAndBookOneMonthAwayAndOpenCachedUrl() {
+
+        int flight = 0;
+
+        String customDate = Helper.getDateWithSpecificMonthsInFuture(Constants.ONE_MONTH,"yyyy-MM-dd");
+        String fullUrl = BaseClass.OOJO_URL+Helper.getFlightSearchResultOneWay(
+                FlightCodes.DALLAS_CODE,
+                FlightCodes.LAHORE_CODE, // Lahore
+                customDate);
+
+        logWrite.info("Open direct search url " + fullUrl);
+        baseClass.openPage(
+                fullUrl);
+
+        searchResultPageObject.waitForSearchLoad();
+        logWrite.info("Accept cookies if there are any");
+        headerPageObject.acceptCookies();
+        headerPageObject.cancelMemberOffer();
+        String pQFlightPrice = searchResultPageObject.getTripOptionPriceByIndex(flight).getText();
+        logWrite.info("Select trip");
+        headerPageObject.cancelMemberOffer();
+        searchResultPageObject.selectTripOptionPq(flight);
+
+        logWrite.info("Assert that price from the list is equal with the price in overview screen");
+        String price = pqTripDetailedViewPageObject.getDetailedViewFlightPrice().getText();
+        Assert.assertEquals(price,pQFlightPrice);
+
+        logWrite.info("Click on book flight");
+        headerPageObject.cancelMemberOffer();
+        pqTripDetailedViewPageObject.clickBookFlight();
+        headerPageObject.waitForLoadingBeeToLoad();
+        String cachedUrl = baseClass.getCurrentUrl();
+
+        String totalPrice = bookPageObject.getTotalPrice().getText();
+        logWrite.info("Check that price from the search result matches the price on the book screen " + totalPrice);
+        Assert.assertEquals(totalPrice,pQFlightPrice, "Price from flight form search result does not match price from book screen");
+
+        bookPageObject.clickOnCheckMoreFlights();
+
+        logWrite.info("Open direct search url " + cachedUrl);
+        baseClass.openPage(cachedUrl);
+        headerPageObject.waitForLoadingBeeToLoad();
+
+        String totalPriceSecondTime = bookPageObject.getTotalPrice().getText();
+        logWrite.info("Check that price from the search result matches the price on the book screen " + totalPriceSecondTime);
+        Assert.assertEquals(totalPriceSecondTime,pQFlightPrice, "Price from flight form search result does not match price from book screen");
+    }
+
+    @Test
+    public void searchNewYorkToMiamiAndBookFourMonthsAwayAndOpenCachedUrl() {
+
+        int flight = 0;
+
+        String customDate = Helper.getDateWithSpecificMonthsInFuture(Constants.FOUR_MONTHS,"yyyy-MM-dd");
+        String fullUrl = BaseClass.OOJO_URL+Helper.getFlightSearchResultOneWay(
+                FlightCodes.NEW_YORK_CODE,
+                FlightCodes.MIAMI,
+                customDate);
+
+        logWrite.info("Open direct search url " + fullUrl);
+        baseClass.openPage(
+                fullUrl);
+
+        searchResultPageObject.waitForSearchLoad();
+        logWrite.info("Accept cookies if there are any");
+        headerPageObject.acceptCookies();
+        headerPageObject.cancelMemberOffer();
+        String pQFlightPrice = searchResultPageObject.getTripOptionPriceByIndex(flight).getText();
+        logWrite.info("Select trip");
+        headerPageObject.cancelMemberOffer();
+        searchResultPageObject.selectTripOptionPq(flight);
+
+        logWrite.info("Assert that price from the list is equal with the price in overview screen");
+        String price = pqTripDetailedViewPageObject.getDetailedViewFlightPrice().getText();
+        Assert.assertEquals(price,pQFlightPrice);
+
+        logWrite.info("Click on book flight");
+        headerPageObject.cancelMemberOffer();
+        pqTripDetailedViewPageObject.clickBookFlight();
+        headerPageObject.waitForLoadingBeeToLoad();
+        String cachedUrl = baseClass.getCurrentUrl();
+
+        String totalPrice = bookPageObject.getTotalPrice().getText();
+        logWrite.info("Check that price from the search result matches the price on the book screen " + totalPrice);
+        Assert.assertEquals(totalPrice,pQFlightPrice, "Price from flight form search result does not match price from book screen");
+
+        bookPageObject.clickOnCheckMoreFlights();
+
+        logWrite.info("Open direct search url " + cachedUrl);
+        baseClass.openPage(cachedUrl);
+        headerPageObject.waitForLoadingBeeToLoad();
+
+        String totalPriceSecondTime = bookPageObject.getTotalPrice().getText();
+        logWrite.info("Check that price from the search result matches the price on the book screen " + totalPriceSecondTime);
+        Assert.assertEquals(totalPriceSecondTime,pQFlightPrice, "Price from flight form search result does not match price from book screen");
+    }
+
+    @Test
+    public void searchFromMcCarranToCansasAndBookTenDaysAwayAndOpenCachedUrl() {
+        int flight = 0;
+
+        String customDate = Helper.getDateWithSpecificDaysInFuture(Constants.TEN_DAYS,"yyyy-MM-dd");
+        String fullUrl = BaseClass.OOJO_URL+Helper.getFlightSearchResultOneWay(
+                FlightCodes.MCCARRAN_INIT_CODE,
+                FlightCodes.CANSASCITY, customDate);
+
+        logWrite.info("Open direct search url " + fullUrl);
+        baseClass.openPage(
+                fullUrl);
+
+        searchResultPageObject.waitForSearchLoad();
+        logWrite.info("Accept cookies if there are any");
+        headerPageObject.acceptCookies();
+        headerPageObject.cancelMemberOffer();
+        String pQFlightPrice = searchResultPageObject.getTripOptionPriceByIndex(flight).getText();
+        logWrite.info("Select trip");
+        headerPageObject.cancelMemberOffer();
+        searchResultPageObject.selectTripOptionPq(flight);
+
+        logWrite.info("Assert that price from the list is equal with the price in overview screen");
+        String price = pqTripDetailedViewPageObject.getDetailedViewFlightPrice().getText();
+        Assert.assertEquals(price,pQFlightPrice);
+
+        logWrite.info("Click on book flight");
+        headerPageObject.cancelMemberOffer();
+        pqTripDetailedViewPageObject.clickBookFlight();
+        headerPageObject.waitForLoadingBeeToLoad();
+        String cachedUrl = baseClass.getCurrentUrl();
+
+        String totalPrice = bookPageObject.getTotalPrice().getText();
+        logWrite.info("Check that price from the search result matches the price on the book screen " + totalPrice);
+        Assert.assertEquals(totalPrice,pQFlightPrice, "Price from flight form search result does not match price from book screen");
+
+        bookPageObject.clickOnCheckMoreFlights();
+
+        logWrite.info("Open direct search url " + cachedUrl);
+        baseClass.openPage(cachedUrl);
+        headerPageObject.waitForLoadingBeeToLoad();
+
+        String totalPriceSecondTime = bookPageObject.getTotalPrice().getText();
+        logWrite.info("Check that price from the search result matches the price on the book screen " + totalPriceSecondTime);
+        Assert.assertEquals(totalPriceSecondTime,pQFlightPrice, "Price from flight form search result does not match price from book screen");
     }
 
 
